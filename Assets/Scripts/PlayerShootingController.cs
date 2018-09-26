a0811f73cc999426580c909524ce3d6f7a4607f2
princegiraffe <32228830+princegiraffe@users.noreply.github.com>
2018-09-26 01:25:58 -0500

    more gun stuff

---
+++ Assets/Scripts/PlayerShootingController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerShootingController : MonoBehaviour
+{
+    public float Range = 100;
+
+    private Camera _camera;
+    private ParticleSystem _particle;
+    private LayerMask _shootableMask;
+
+    void Start()
+    {
+        _camera = Camera.main;
+        _particle = GetComponentInChildren<ParticleSystem>();
+        Cursor.lockState = CursorLockMode.Locked;
+        _shootableMask = LayerMask.GetMask("Shootable");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit, Range, _shootableMask))
+            {
+                print("hit " + hit.collider.gameObject);
+                _particle.Play();
+
+                EnemyHealth health = hit.collider.GetComponent<EnemyHealth>();
+                if (health != null)
+                {
+                    health.TakeDamage(1);
+                }
+            }
+        }
+    }
+}
