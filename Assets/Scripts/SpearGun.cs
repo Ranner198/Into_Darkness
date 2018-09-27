8a732932f54bc641fb2d17e2caf9f9960c3f6d4b
Ranner <rancrump18@gmail.com>
2018-09-27 11:04:17 -0500

    +Alpha V1.1 +Where is Frandado?

--- Assets/Scripts/SpearGun.cs
+++ Assets/Scripts/SpearGun.cs
@@ -4,7 +4,8 @@ public class SpearGun : MonoBehaviour {
 
     public float damage = 10f;
     public float range = 100f;
-
+    public GameObject spear;
+    public GameObject spawnPoint;
     public Camera fpsCam;
 	
 	// Update is called once per frame
@@ -19,10 +20,16 @@ public class SpearGun : MonoBehaviour {
 
     void Shoot()
     {
+        /*
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
         }
+        */
+
+        Object clone = Instantiate(spear, spawnPoint.transform.position, fpsCam.transform.rotation);
+        Destroy(clone, 3.0f);
+
     }
 }
