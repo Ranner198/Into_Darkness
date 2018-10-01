bf066c4e057e34e84528e515181c32fb1badb37f
princegiraffe <32228830+princegiraffe@users.noreply.github.com>
2018-10-01 18:38:52 -0500

    Added animation for main menu

---
+++ Assets/Scripts/AmmoItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoItem : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            other.GetComponent<SpearGun>().ammo += 100;
+            Destroy(gameObject);
+        }
+    }
+}
