65b534418a323cbf1f1b37a8d302124d23152fd0
princegiraffe <32228830+princegiraffe@users.noreply.github.com>
2018-09-30 23:57:35 -0500

    Added ammo, ammotext and ammopickup item

--- Assets/Scripts/SpearGun.cs
+++ Assets/Scripts/SpearGun.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SpearGun : MonoBehaviour {
 
     public float damage = 10f;
     public float range = 100f;
+    public int ammo = 5;
+    public Text ammoText;
     public GameObject spear;
     public GameObject spawnPoint;
     public Camera fpsCam;
 	
 	// Update is called once per frame
 	void Update () {
+
+        DisplayAmmo();
 
         if (Input.GetButtonDown("Fire1"))
         {
@@ -28,8 +33,18 @@ public class SpearGun : MonoBehaviour {
         }
         */
 
-        Object clone = Instantiate(spear, spawnPoint.transform.position, fpsCam.transform.rotation);
-        Destroy(clone, 3.0f);
+        //check to make sure there is enough ammo
+        if (ammo > 0)
+        {
+            Object clone = Instantiate(spear, spawnPoint.transform.position, fpsCam.transform.rotation);
+            Destroy(clone, 3.0f);
+        }
 
     }
+
+    void DisplayAmmo ()
+    {
+        ammoText.text = "Ammo: " + ammo;
+    }
+
 }
--- Assets/Scripts/oxygenItems.cs
+++ Assets/Scripts/oxygenItems.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class oxygenItems : MonoBehaviour {
+public class OxygenItems : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
