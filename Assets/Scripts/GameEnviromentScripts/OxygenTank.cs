866419b71c50885fca080c91d133f5f5ce23b291
princegiraffe <32228830+princegiraffe@users.noreply.github.com>
2018-10-09 09:11:18 -0500

    Stuff

---
+++ Assets/Scripts/GameEnviromentScripts/OxygenTank.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenTank : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            other.GetComponent<OxygenSystem>().startingOxygen += 100;
+            Destroy(gameObject);
+        }
+    }
+}
+
