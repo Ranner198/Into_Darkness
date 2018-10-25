93a62bd485dd3631a7e40fa863a06f76941a69ef
princegiraffe <32228830+princegiraffe@users.noreply.github.com>
2018-10-24 23:00:24 -0500

    Spawners

---
+++ Assets/Scripts/GameEnviromentScripts/EarthquakeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthquakeSpawner : MonoBehaviour {
+
+    //prefabs to instantiate
+    public GameObject earthquakePrefab;
+
+    //location of the spawn points
+    public GameObject[] spawnPoints;
+
+    //variable to keep track of spawns and update index
+    int spawnCounter = 0;
+
+    void Start()
+    {
+        spawnItems();
+    }
+
+    void spawnItems()
+    {
+
+        while (spawnCounter < spawnPoints.Length - 1)
+        {
+            Instantiate(earthquakePrefab, spawnPoints[spawnCounter].transform.position, Quaternion.identity);
+            spawnCounter++;
+        }
+    }
+}
---
+++ Assets/Scripts/GameEnviromentScripts/PlayerSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject[] spawnPoints;
+    int chooser;
+
+    void Start()
+    {
+        chooser = Random.Range(0, spawnPoints.Length - 1);
+
+        player.transform.position = spawnPoints[chooser].transform.position;
+    }
+}
