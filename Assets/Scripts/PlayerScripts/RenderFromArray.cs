3111180a6af2d9fc110e8ff7fb5e386cd43fa9e0
Ranner198 <rancrump18@gmail.com>
2018-10-27 15:40:48 -0500

    7SecondDisplayAdd

---
+++ Assets/Scripts/PlayerScripts/RenderFromArray.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderFromArray : MonoBehaviour {
+
+    public Sprite[] numbers;
+    public GameObject[] spriteObjects;
+    public SpriteRenderer[] SR;
+
+    //Amt Variables
+    public float mins;
+    public float tens;
+
+    void Start()
+    {
+        //Get all the sprite renderer componetes to access the sprites
+        for (int i = 0; i < spriteObjects.Length; i++)
+        {
+            SR[i] = spriteObjects[i].GetComponent<SpriteRenderer>();
+        }
+    }
+
+    void Update() {
+
+        //Update temp variable to Timer
+        var temp = PlayerMovement.player.GetOxygenLevel();
+        Debug.Log(PlayerMovement.player.GetOxygenLevel());
+        mins = 0;
+        tens = 0;
+
+        SR[0].GetComponent<SpriteRenderer>().sprite = numbers[0];
+
+        while (temp >= 60)
+        {
+            temp -= 60;
+            mins++;
+        }
+        while (temp >= 10)
+        {
+            temp -= 10;
+            tens++;
+        }
+        Debug.Log(temp);
+        SR[1].GetComponent<SpriteRenderer>().sprite = numbers[Mathf.FloorToInt(mins)];
+        SR[2].GetComponent<SpriteRenderer>().sprite = numbers[Mathf.FloorToInt(tens)];
+        SR[3].GetComponent<SpriteRenderer>().sprite = numbers[Mathf.FloorToInt(temp)];
+	}
+}
