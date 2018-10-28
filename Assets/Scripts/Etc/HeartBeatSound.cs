81dc25eebaa267d09202f790b1dd16dd62210833
Ranner198 <rancrump18@gmail.com>
2018-10-28 15:16:49 -0500

    Becon Effects

--- Assets/Scripts/Etc/HeartBeatSound.cs
+++ Assets/Scripts/Etc/HeartBeatSound.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class HeartBeatSound : MonoBehaviour {
 
-    public AudioClip[] Sound;
+    public AudioClip Sound;
     public AudioSource audioSource;
 
     private bool isPlaying = false;
-    private float timer, setter = 10;
     private GameObject player;
-
-    public EnemyClass shark;
 
     void Start () {
         audioSource = GetComponent<AudioSource>();
@@ -21,19 +18,17 @@ public class HeartBeatSound : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float distance = (transform.position - player.transform.position).magnitude;
-        if (distance < 35)
+        if (distance < 35 && !isPlaying)
         {
+            print("Playing");
             isPlaying = true;
-            audioSource.PlayOneShot(Sound[0], 1.0f);
+            audioSource.PlayOneShot(Sound, 4);
         }
-
-        if (timer >= 0 && isPlaying)
-            timer -= Time.deltaTime;
-
-        if (timer < 0 && isPlaying)
-        {   
+        if (distance > 35 && isPlaying) 
+        {
+            print("Stopped");
             isPlaying = false;
-            timer = setter;
+            audioSource.Stop();
         }
     }
 }
--- Assets/Scripts/GameEnviromentScripts/BeaconDirection.cs
+++ Assets/Scripts/GameEnviromentScripts/BeaconDirection.cs
@@ -5,34 +5,56 @@ using UnityEngine;
 public class BeaconDirection : MonoBehaviour {
 
     private GameObject target, player;
+    private Terrain terrain;
+    private bool play;
+    private GameObject mover;
+    private SpriteRenderer SR;
+    private int counter = 0;
+    private Vector3 dir;
 
-    public string subName = "Sub", playerName = "Player";
-
-    public Terrain terrain;
-
-    private float Distance;
+    public AudioClip beaconSound;
+    AudioSource audioSource;
 
     void Start () {
-        target = GameObject.FindGameObjectWithTag(subName);
-        player = GameObject.FindGameObjectWithTag(playerName).transform.GetChild(2).gameObject;
+        terrain = FindObjectOfType<Terrain>();
+        player = GameObject.FindWithTag("Player").transform.GetChild(2).gameObject;
+        mover = gameObject.transform.GetChild(0).gameObject;
+        SR = mover.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
     }
 
 	void Update () {
-        /*
-        Vector3 Direction = -(player.transform.position - target.transform.position).normalized;
 
-        Distance = (player.transform.position - target.transform.position).magnitude;
-
-        float MapHypotnuse = Mathf.Sqrt(Mathf.Pow(terrain.terrainData.size.x, 2) + (Mathf.Pow(terrain.terrainData.size.z, 2)));
-
-        if (Distance > MapHypotnuse/2)
-        {
-            Debug.DrawRay(player.transform.position, -Direction * 9, Color.red);
-        }
+        if (target == null)
+            target = GameObject.FindGameObjectWithTag("Sub");
         else
         {
-            Debug.DrawRay(player.transform.position, Direction * 9, Color.red);
+           mover.transform.LookAt(target.transform);
         }
-        */
+
+        SR.enabled = play ? true : false;
+    }
+
+    private void OnTriggerStay(Collider coll)
+    {
+        counter++;
+        if (coll.gameObject == player)
+        {
+            if (counter % 50 == 0)
+            {
+                play = !play;
+                audioSource.PlayOneShot(beaconSound);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        counter = 0;
+        if (coll.gameObject == player)
+        {
+            play = false;
+            audioSource.Stop();
+        }
     }
 }
