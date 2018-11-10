26d1c9180b03edd1598b2a5ddf34c4b87d266d88
Ranner198 <rancrump18@gmail.com>
2018-11-09 22:58:00 -0600

    Dank Playtest Time

--- Assets/Scripts/MainMenuScripts/MenuSettings.cs
+++ Assets/Scripts/MainMenuScripts/MenuSettings.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 public class MenuSettings : MonoBehaviour {
 
     public AudioMixer audiomixer;
+    public float startingVolume = -25;
+
+    private void Start()
+    {
+        audiomixer.SetFloat("Volume", -25);
+    }
 
     public void VolumePercentage(float vol) {
-        audiomixer.SetFloat("volume", vol);
+        audiomixer.SetFloat("Volume", vol);
         Debug.Log(vol);
     }
 
