c0beb331bc15b349a8ed5775aa01484e4beafcc5
Fernando Palacios <fp0003@uah.edu>
2018-11-10 15:55:09 -0600

    Sea Monster

--- Assets/Scripts/EnemyScripts/SeaMonsterScript.cs
+++ Assets/Scripts/EnemyScripts/SeaMonsterScript.cs
@@ -5,35 +5,52 @@ using UnityEngine;
 public class SeaMonsterScript : MonoBehaviour {
 
     public EnemyClass monster = new EnemyClass(12, 100);
-    public bool debugMode = false;
+    public float time = 10;
 
     private GameObject player;
-    private Terrain terrain;   
-    private Rigidbody rb;
-    private Animator anim;
+    private Terrain terrain;
+    private Rigidbody rigidBody;
+    private Animator animator;
 
     // Use this for initialization
     void Start () {
-        monster.SetState(0);
+        monster.SetState(2);
+        monster.GenerateAggro();
 
         if (player == null)
             player = GameObject.FindGameObjectWithTag("Player").transform.GetChild(2).gameObject;
         if (terrain == null)
             terrain = FindObjectOfType<Terrain>();
 
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody>();
+        timer();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (monster.GetState() == 0)
+        if (monster.GetState() == 2)
         {
-            //passive
-            monster.PassiveFix(player, gameObject, terrain, 55);
-            if (debugMode)
-                print("Passive");
-            anim.SetFloat("Direction", 1);
-            anim.Play("Passive", 1, float.NegativeInfinity);
+            monster.Attack(player, gameObject, terrain, 5f);
+            animator.SetFloat("Direction", 1);
+            animator.Play("Passive", 1, float.NegativeInfinity);
+
+            StartCoroutine(BiteAndRun());
         }
+    }
+
+    IEnumerator BiteAndRun()
+    {
+        yield return new WaitForSeconds(1.15f);
+        monster.SetState(5);
+        animator.Play("Passive");
+        yield return new WaitForSeconds(.5f);
+        monster.SetState(3);
+        yield return new WaitForSeconds(8f);
+        monster.SetState(0);
+    }
+
+    void timer() {
+        time -= Time.deltaTime;
     }
 }
