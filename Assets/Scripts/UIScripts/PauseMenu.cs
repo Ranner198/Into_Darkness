54f70e210cb21e18d2880a52df7f510fbdee6bd0
Fernando Palacios <fp0003@uah.edu>
2018-11-13 09:17:24 -0600

    Added menus

---
+++ Assets/Scripts/UIScripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool gamePaused = false;
+
+    public GameObject pauseMenuScreen;
+    public GameObject optionsMenuScreen;
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+
+        }
+	}
+
+    void Pause()
+    {
+        pauseMenuScreen.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuScreen.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Game_Scene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        pauseMenuScreen.SetActive(false);
+        optionsMenuScreen.SetActive(true);
+    }
+
+    public void Back()
+    {
+        pauseMenuScreen.SetActive(true);
+        optionsMenuScreen.SetActive(false);
+    }
+}
